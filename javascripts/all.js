e00ed90c5bd6695e0c93f91dead0e7e33bdf96f3
Shawna C Scott <shawnatyger@gmail.com>
2014-01-13 20:04:44 -0800

    Push more content

--- javascripts/all.js
+++ javascripts/all.js
@@ -1,1 +1,2 @@
+// Copyright (c) 2013, 2014 ICRL
 $(document).ready(function(){$(".answer").hide(),$(".question h4 a").click(function(e){e.preventDefault(),$(this).parent().next().slideToggle()})});
